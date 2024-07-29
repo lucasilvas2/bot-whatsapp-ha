2ce6c58bf956ab665d0e3f2085a604384ecdabdb
LUCASNOTE\lucas <lucasoliver1068@gmail.com>
2024-07-28 22:26:00 -0300

    first commit

---
+++ bot-whatsapp/index.js
@@ -0,0 +1,116 @@
+const { Client } = require('whatsapp-web.js');
+const axios = require('axios');
+
+// const client = new Client();
+
+const client = new Client({
+    // authStrategy: new LocalAuth(),
+    // proxyAuthentication: { username: 'username', password: 'password' },
+    puppeteer: {
+        // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
+        headless: false,
+    }
+});
+
+// client initialize does not finish at ready now.
+client.initialize();
+
+client.on('loading_screen', (percent, message) => {
+    console.log('LOADING SCREEN', percent, message);
+});
+
+// Pairing code only needs to be requested once
+let pairingCodeRequested = false;
+client.on('qr', async (qr) => {
+    // NOTE: This event will not be fired if a session is specified.
+    console.log('QR RECEIVED', qr);
+
+    // paiuting code example
+    const pairingCodeEnabled = false;
+    if (pairingCodeEnabled && !pairingCodeRequested) {
+        const pairingCode = await client.requestPairingCode('5584987631700'); // enter the target phone number
+        console.log('Pairing code enabled, code: '+ pairingCode);
+        console.log('Number: ' + '558491097019');
+        pairingCodeRequested = true;
+    }
+});
+
+client.on('authenticated', () => {
+    console.log('AUTHENTICATED');
+});
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessful
+    console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('ready', async () => {
+    console.log('READY');
+    const debugWWebVersion = await client.getWWebVersion();
+    console.log(`WWebVersion = ${debugWWebVersion}`);
+
+    console.log(client);
+    client.pupPage.on('pageerror', function(err) {
+        console.log('Page error: ' + err.toString());
+    });
+    client.pupPage.on('error', function(err) {
+        console.log('Page error: ' + err.toString());
+    });
+
+});
+
+client.on('message_create', async (msg) => {
+    // Fired on all message creations, including your own
+    if (msg.fromMe) {
+        // do stuff here
+    }
+
+    // Unpins a message
+    if (msg.fromMe && msg.body.startsWith('!unpin')) {
+        const pinnedMsg = await msg.getQuotedMessage();
+        if (pinnedMsg) {
+            // Will unpin a message
+            const result = await pinnedMsg.unpin();
+            console.log(result); // True if the operation completed successfully, false otherwise
+        }
+    }
+
+    if (msg.body === '!ping') {
+        // send back "pong" to the chat the message was sent in
+        await axios({
+            method: 'post',
+            url: 'http://localhost:5000/webhook',
+            data: {
+                topic: 'Fred',
+                target: 'Flintstone',
+                value: 545454
+            }
+        }).then((response) => {
+            console.log(response.data);
+        }).catch((err) => {
+            console.error(err);
+        });
+
+        await msg.reply('pong');
+    }
+
+    if(msg.body.includes('!tv')) {
+        let text = msg.body.replace('!', '');
+        let [topic, value] = text.split(' ');
+        let data = {
+            topic: topic,
+            value: value
+        };
+
+        await axios({
+            method: 'post',
+            url: 'http://localhost:5000/webhook',
+            data: data
+        }).then((response) => {
+            console.log(response.data.message);
+        }).catch((err) => {
+            console.error(err);
+        });
+    }
+
+});
---
+++ mqtt-webhook/webhook.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+import paho.mqtt.client as mqtt
+
+app = Flask(__name__)
+THE_BROKER = "192.168.15.11"
+CLIENT_ID = ""
+
+@app.route('/webhook', methods=['POST'])
+def webhook_receiver():
+    data = request.json  # Extract JSON data from the request
+    client = init_publisher_client()
+    publisher_message(client, data.get("topic"), data.get("value"))
+    print("Received webhook data:", data)
+    return jsonify({'message': 'Webhook received successfully'}), 200
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to ", client._host, "port: ", client._port)
+    print("Flags: ", flags, "returned code: ", rc)
+
+
+def on_publish(client, userdata, mid):
+    print("sipub: msg published (mid={})".format(mid))
+
+
+def init_publisher_client():
+    client = mqtt.Client(client_id=CLIENT_ID,
+                         clean_session=True,
+                         userdata=None,
+                         protocol=mqtt.MQTTv311,
+                         transport="tcp")
+
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+
+    client.username_pw_set(None, password=None)
+    client.connect(THE_BROKER, port=1883, keepalive=60)
+    return client
+
+
+def publisher_message(client, topic, msg, qos=0, retain=False):
+    client.publish(topic,
+                   payload=msg,
+                   qos=qos,
+                   retain=retain)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
